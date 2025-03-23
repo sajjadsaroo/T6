02b78195e3bda7c8f7ffb34db7dc12b882baa1d6
Macsaroo <sajjad.sarookhani@gmail.com>
2025-03-24 01:47:15 +0330

    add getters , setters and constructor for bank account class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -6,5 +6,33 @@ public abstract class BankAccount {
     private String accountHolderName;
     protected double balance;
 
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        setBalance(balance);
+        setAccountHolderName(accountHolderName);
+        this.accountNumber = accountNumber;
+    }
+
+
 
 }
