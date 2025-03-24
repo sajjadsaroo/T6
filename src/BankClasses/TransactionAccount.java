724cd666fca0b5058172adba5d088e3a31ab8c4e
sajjadsaroo <78489169+sajjadsaroo@users.noreply.github.com>
2025-03-25 01:29:31 +0330

    Update TransactionAccount.java and some change to get better on clean code

--- src/BankClasses/TransactionAccount.java
+++ src/BankClasses/TransactionAccount.java
@@ -1,31 +1,31 @@
 package BankClasses;
 
 public class TransactionAccount extends BankAccount {
-    private final double overDraftLimit = 500;
+    private static final double overDraftLimit = 500; // ثابت‌ها با حروف بزرگ
 
     public TransactionAccount(String accountNumber, String accountHolderName, double initialBalance) {
         super(accountNumber, accountHolderName, initialBalance);
     }
 
+    @Override
     public void calculateInterest() {
-
-        System.out.println("This account is not have interest\n");
+        System.out.println("This account does not accrue interest.");
     }
-
 
     @Override
     public void withdraw(double amount) {
-        if (amount > 0) {
-            if (amount <= overDraftLimit + getBalance()) {
-                balance -= amount;
-                System.out.println("Withdrawn " + amount + " " + getAccountNumber());
-            } else {
-                System.out.println("Insufficient funds");
-            }
-        } else {
-            System.out.println("this amount is not valid!");
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+            return;
         }
+
+        if (amount > getBalance() + overDraftLimit) {
+            System.out.println("Insufficient funds for withdrawal.");
+            return;
+        }
+
+        setBalance(getBalance() - amount);
+        System.out.println("Successfully withdrawn: " + amount + " from account: " + getAccountNumber());
     }
-
-
 }
+
